3a85bf0a684a56aa8c4efbd8268769f50d67d97b
James <jameskutw@gmail.com>
2023-09-23 11:37:52 +0800

    Add maybe implementation

---
+++ monad-video/js/script/7-maybe.ts
@@ -0,0 +1,52 @@
+class Maybe<T> {
+    private value: T | null;
+  
+    private constructor(value: T | null) {
+      this.value = value;
+    }
+  
+    // Static factory method to create a Maybe instance
+    static just<T>(value: T): Maybe<T> {
+      return new Maybe(value);
+    }
+  
+    // Static factory method for an empty Maybe instance
+    static nothing<T>(): Maybe<T> {
+      return new Maybe<T>(null);
+    }
+  
+    // Apply a function to the value inside Maybe and return a new Maybe
+    map<U>(fn: (value: T) => U): Maybe<U> {
+      if (this.value === null) {
+        return Maybe.nothing();
+      } else {
+        return Maybe.just(fn(this.value));
+      }
+    }
+
+    flatMap<U>(fn: (value: T) => Maybe<U>): Maybe<U> {
+        if (this.value === null) {
+          return Maybe.nothing();
+        } else {
+          return fn(this.value);
+        }
+      }
+  }
+
+function trySomething1(): Maybe<number> {
+    return Maybe.just(1);
+}
+
+function trySomething2(): Maybe<number> {
+    return Maybe.nothing();
+}
+  
+function ex1() {
+    const res = trySomething1().flatMap(x => trySomething2().flatMap(y => Maybe.just(x + y)));
+}
+
+function imaginaryDoNotation(): Maybe<number>{
+    const x = await trySomething1();
+    const y = await trySomething2();
+    return Maybe.just(x + y);
+}
